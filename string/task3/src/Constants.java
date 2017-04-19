b6e91c944fd62e399fdd1f0c55ba7926f12ab801
1nadequite <yaretsaa@gmail.com>
2017-04-19 14:52:10 +0300

    task3

--- string/task3/src/Constants.java
+++ string/task3/src/Constants.java
@@ -1,7 +1,7 @@
 public final class Constants {
     public static final String INPUT_TXT = "src/in.txt";
     public static final String PRICE_REGEX =
-            "(\\p{Blank}|\\b)(\\p{Blank}*([1-9]{1}[0-9]{0,2})" +
+            "(\\p{Blank}?)(\\p{Blank}*([1-9]{1}[0-9]{0,2})" +
                     "((\\p{Blank}+[0-9]{3})*)\\p{Blank}*)" +
                     "\\p{Blank}(belarusian roubles|blr)";
     public static final String DATE_REGEX = "([0-9]?[1-9])[\\.|\\/|\\-]([0-9]?[1-9])[\\.|\\/|\\-][0-9]{0,2}([0-9]{2})";
